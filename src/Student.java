e84308944c4243225765c83303a51fb190180a5b
Sarah Strange <strange1120@gmail.com>
2018-05-31 23:52:50 -0400

    filling out student file

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;
 
 public class Student {
 
 	private String firstName;
 	private String lastName;
-	private String gradeYear;
-	private int studentId;
+	private int gradeYear;
+	private String studentId;
 	private String courses;
 	private int tuitionBalance;
 	private int costOfCourse = 600;
+	private static int id = 1000;
+	
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter student first name");
+		this.firstName = in.nextLine();
+		
+		System.out.println("Enter student last name");
+		this.lastName = in.nextLine();
+		
+		System.out.println("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+		this.gradeYear = in.nextInt();
+		
+		setStudentId();
+
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentId);
+		
+	}
+	
+	private void setStudentId() {
+		id++;
+		studentId = gradeYear + "" + id;
+		
+	}
 	
 }
--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -3,7 +3,7 @@ public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		
-		
+		Student student1 = new Student();
 		
 	}
 
