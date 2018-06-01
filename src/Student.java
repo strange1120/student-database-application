93350690b66e27e3f3045bdefd23caa51bb15f86
Sarah Strange <strange1120@gmail.com>
2018-05-31 23:38:46 -0400

    back to the basics back to the matrix

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+
+public class Student {
+
+	private String firstName;
+	private String lastName;
+	private String gradeYear;
+	private int studentId;
+	private String courses;
+	private int tuitionBalance;
+	private int costOfCourse = 600;
+	
+}
